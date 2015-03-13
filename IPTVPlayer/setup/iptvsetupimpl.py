0f18586d3e532eeb1cc14ad3d9d033a587a4d1d7
samsamsam <samsamsam@o2.pl>
2015-03-13 02:01:19 +0100

    Provides new version of the rtmpdump with small optimization for UDS streams.

--- IPTVPlayer/setup/iptvsetupimpl.py
+++ IPTVPlayer/setup/iptvsetupimpl.py
@@ -52,7 +52,7 @@ class IPTVSetupImpl:
         self.rtmpdumppaths = ["/usr/bin/rtmpdump", "rtmpdump"]
         
         # f4mdump member
-        self.f4mdumpVersion = "F4MDump v0.31"
+        self.f4mdumpVersion = "F4MDump v0.32"
         self.f4mdumppaths = ["/usr/bin/f4mdump", GetBinDir("f4mdump", "")]
         self._f4mdumpInstallChoiseList = [(_('Install into the "%s".') % ("/usr/bin/f4mdump (%s)" % _("recommended")), "/usr/bin/f4mdump"),
                                           (_('Install into the "%s".') % "IPTVPlayer/bin/f4mdump", GetBinDir("f4mdump", "")),
