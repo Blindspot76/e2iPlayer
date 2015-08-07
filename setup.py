3bf934b54e63722d313acf10b6ca31b9a4a85376
samsamsam <samsamsam@o2.pl>
2015-08-07 22:24:01 +0200

    Commit patch prepaded by friend @athoik - thank you: http://forums.openpli.org/topic/38172-iptv-player-development-thread/page-7#entry500331

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import setup_translate
+
+pkg = 'Extensions.IPTVPlayer'
+setup (name = 'enigma2-plugin-extensions-iptvplayer',
+       version = '1.0',
+       description = 'IPTV Player for E2',
+       package_dir = {pkg: 'IPTVPlayer'},
+       packages = [pkg],
+       package_data = {pkg: ['*.*', '*/*.*', '*/*/*.*', '*/*/*/*.*', '*/*/*/*/*.*']},
+       cmdclass = setup_translate.cmdclass, # for translation
+      )
