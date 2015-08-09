b9df10a7bc052310f5afdf609d7de68f421202a6
samsamsam <samsamsam@o2.pl>
2015-08-09 17:12:57 +0200

    Greek translation from satdreamgr team member MCelliot_g.

--- IPTVPlayer/version.py
+++ IPTVPlayer/version.py
@@ -7,4 +7,4 @@
 #zmiana YY powoduje wyzerowanie ZZ
 #zmiana XX powoduje wyzerowanie YY i ZZ
 #usunięcie/zablokowanie hosta nie powoduje zmiany QQ
-IPTV_VERSION="74.01.01.00"
+IPTV_VERSION="74.02.00.00"
