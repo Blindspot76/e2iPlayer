1c069b072fc63a997ccdf9c0f20a873f2252cba9
samsamsam <samsamsam@o2.pl>
2015-12-07 01:46:50 +0100

    Fix link extraction from http://cartoonhd.mobi/

--- IPTVPlayer/hosts/hostcartoonhd.py
+++ IPTVPlayer/hosts/hostcartoonhd.py
@@ -35,9 +35,13 @@ from Screens.MessageBox import MessageBox
 # Config options for HOST
 ###################################################
 config.plugins.iptvplayer.movieshdco_sortby = ConfigSelection(default = "date", choices = [("date", _("Lastest")), ("views", _("Most viewed")), ("duree", _("Longest")), ("rate", _("Top rated")), ("random", _("Tandom"))]) 
+config.plugins.iptvplayer.cartoonhd_login    = ConfigText(default = "", fixed_size = False)
+config.plugins.iptvplayer.cartoonhd_password = ConfigText(default = "", fixed_size = False)
 
 def GetConfigList():
     optionList = []
+    optionList.append(getConfigListEntry(_("login")+":", config.plugins.iptvplayer.cartoonhd_login))
+    optionList.append(getConfigListEntry(_("password")+":", config.plugins.iptvplayer.cartoonhd_password))
     return optionList
 ###################################################
 
@@ -66,10 +70,11 @@ class CartoonHD(CBaseHostClass):
                     #                    {'sort_by':'trending',    'title':'Trending'}
  
     def __init__(self):
-        CBaseHostClass.__init__(self, {'history':'CartoonHD.tv', 'cookie':'cartoonhdtv.cookie'})
+        CBaseHostClass.__init__(self, {'history':'CartoonHD.tv', 'cookie':'cartoonhdtv.cookie', 'cookie_type':'MozillaCookieJar'})
         self.defaultParams = {'use_cookie': True, 'load_cookie': True, 'save_cookie': True, 'cookiefile': self.COOKIE_FILE}
         self.cacheFilters = {}
         self.cacheLinks = {}
+        self.loggedIn = None
         
     def _getFullUrl(self, url):
         if 0 < len(url) and not url.startswith('http'):
@@ -96,7 +101,7 @@ class CartoonHD(CBaseHostClass):
     def fillCategories(self):
         printDBG("CartoonHD.fillCategories")
         self.cacheFilters = {}
-        sts, data = self.cm.getPage(self.MAIN_URL)
+        sts, data = self.cm.getPage(self.MAIN_URL, self.defaultParams)
         if not sts: return
         
         moviesTab = [{'title':'All', 'url':self._getFullUrl('movies')}]
@@ -153,7 +158,9 @@ class CartoonHD(CBaseHostClass):
         
         url = cItem['url'] + '/' + cItem.get('sort_by', '') + '/' + str(page)
         
-        sts, data = self.cm.getPage(url, {'header':self.AJAX_HEADER})
+        params = dict(self.defaultParams)
+        params['header'] = self.AJAX_HEADER
+        sts, data = self.cm.getPage(url, params)
         if not sts: return
         
         nextPage = self.cm.ph.getDataBeetwenMarkers(data, '<label for="pagenav">', '</form>', False)[1]
@@ -187,7 +194,7 @@ class CartoonHD(CBaseHostClass):
     def listSeasons(self, cItem, nextCategory):
         printDBG("CartoonHD.listSeasons")
 
-        sts, data = self.cm.getPage(cItem['url'])
+        sts, data = self.cm.getPage(cItem['url'], self.defaultParams)
         if not sts: return
         
         data = self.cm.ph.getDataBeetwenMarkers(data, '<div class="tab selected" id="episodes">', '</select>', False)[1]
@@ -201,7 +208,7 @@ class CartoonHD(CBaseHostClass):
     def listEpisodes(self, cItem):
         printDBG("CartoonHD.listEpisodes")
 
-        sts, data = self.cm.getPage(cItem['url'])
+        sts, data = self.cm.getPage(cItem['url'], self.defaultParams)
         if not sts: return
         
         data = self.cm.ph.getDataBeetwenMarkers(data, '<li class="episode ">', '</ul>', False)[1]
@@ -307,7 +314,8 @@ class CartoonHD(CBaseHostClass):
         sts, data = self.cm.getPage(cItem['url'], self.defaultParams)
         if not sts: return []
         
-        tor  = self.cm.ph.getSearchGroups(data, "tor='([^']+?)'")[0]
+        torName = self.cm.ph.getSearchGroups(data, "var token=([^;]+?);")[0]
+        tor  = self.cm.ph.getSearchGroups(data, "{0}='([^']+?)'".format(torName))[0]
         elid = self.cm.ph.getSearchGroups(data, 'data-movie="([^"]+?)"')[0]
         if '' == elid: elid = self.cm.ph.getSearchGroups(data, 'elid="([^"]+?)"')[0]
         if '' == elid: return []
@@ -317,26 +325,29 @@ class CartoonHD(CBaseHostClass):
         for item in data:
             hostings.append({'id':item[0], 'name':item[1]})
         
-        httpParams = {} #dict(self.defaultParams)
+        httpParams = dict(self.defaultParams)
         httpParams['header'] =  {'Referer':cItem['url'], 'User-Agent':self.cm.HOST, 'X-Requested-With':'XMLHttpRequest', 'Accept':'application/json, text/javascript, */*; q=0.01'}
         if '/movie/' in cItem['url']:
             type = 'getMovieEmb'
         else: type = 'getEpisodeEmb'
         encElid = gettt()
-        httpParams['header']['Cookie'] = '%s=%s; PHPSESSID=%s; flixy=%s;'% (elid, urllib.quote(encElid), getCookieItem('PHPSESSID'), getCookieItem('flixy'))
+        __utmx = getCookieItem('__utmx')
+        httpParams['header']['Authorization'] = 'Bearer ' + __utmx
+        
+        #httpParams['header']['Cookie'] = '%s=%s; PHPSESSID=%s; flixy=%s;'% (elid, urllib.quote(encElid), getCookieItem('PHPSESSID'), getCookieItem('flixy'))
         url = 'ajax/embeds.php'
         post_data = {'action':type, 'idEl':elid, 'token':tor, 'elid':urllib.quote(encElid)}
         sts, data = self.cm.getPage(self._getFullUrl(url), httpParams, post_data)
         if not sts: return []
-        #printDBG('===============================================================')
-        #printDBG(data)
-        #printDBG('===============================================================')
-        #printDBG(hostings)
+        printDBG('===============================================================')
+        printDBG(data)
+        printDBG('===============================================================')
+        printDBG(hostings)
         try:
             data = byteify(json.loads(data))
             for item in hostings:
                 if item['id'] in data:
-                    url = data[item['id']].replace('\\/', '/')
+                    url = data[item['id']]['embed'].replace('\\/', '/')
                     url = self.cm.ph.getDataBeetwenMarkers(url, 'src="', '"', False, False)[1]
                     if 'googlevideo.com' in url or 'googleusercontent.com' in url:
                         need_resolve = 0
@@ -364,8 +375,34 @@ class CartoonHD(CBaseHostClass):
     def getLinksForFavourite(self, fav_data):
         return self.getLinksForVideo({'url':fav_data})
 
+    def tryTologin(self):
+        printDBG('tryTologin start')
+        login = config.plugins.iptvplayer.cartoonhd_login.value
+        password = config.plugins.iptvplayer.cartoonhd_password.value
+        
+        if '' == login.strip() or '' == password.strip():
+            printDBG('tryTologin wrong login data')
+            #self.sessionEx.open(MessageBox, _('This host requires registration. \nPlease fill your login and password in the host configuration. Available under blue button.'), type = MessageBox.TYPE_ERROR, timeout = 10 )
+            return False
+        
+        post_data = {'username':login, 'password':password, 'action':'login', 't':''}
+        params = {'header':self.HEADER, 'cookiefile':self.COOKIE_FILE, 'use_cookie': True, 'save_cookie':True}
+        sts, data = self.cm.getPage(self.MAIN_URL, params, post_data)
+        if sts:
+            if '>Logout<' in data:
+                printDBG('tryTologin OK')
+                return True
+     
+        self.sessionEx.open(MessageBox, _('Login failed.'), type = MessageBox.TYPE_ERROR, timeout = 10)
+        printDBG('tryTologin failed')
+        return False
+        
+    
     def handleService(self, index, refresh = 0, searchPattern = '', searchType = ''):
         printDBG('handleService start')
+        
+        if None == self.loggedIn:
+            self.loggedIn = self.tryTologin()
         
         CBaseHostClass.handleService(self, index, refresh, searchPattern, searchType)
 
