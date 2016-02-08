81d667f2215e65eb2f9a7c61154eb0ec495c008e
samsamsam <samsamsam@o2.pl>
2016-02-08 01:47:03 +0100

    Fix last commit.

---
+++ IPTVPlayer/version.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#QQ.XX.YY.ZZ
+#QQ-dodanie nowego hosta
+#XX-dodanie nowej funkcjonalnosci
+#YY-poprawka dzialania
+#ZZ-poprawki kosmetyczne 
+#zmiana YY powoduje wyzerowanie ZZ
+#zmiana XX powoduje wyzerowanie YY i ZZ
+#usunięcie/zablokowanie hosta nie powoduje zmiany QQ
+IPTV_VERSION="102.00.00.00"
