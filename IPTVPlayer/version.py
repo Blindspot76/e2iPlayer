27a9363bf18951cdec64ef93cf6637e3c61b49da
samsamsam <samsamsam@o2.pl>
2016-05-03 23:35:22 +0200

    http://ustream.tv/ dwonload video stream improvment.

--- IPTVPlayer/version.py
+++ IPTVPlayer/version.py
@@ -7,4 +7,4 @@
 #zmiana YY powoduje wyzerowanie ZZ
 #zmiana XX powoduje wyzerowanie YY i ZZ
 #usunięcie/zablokowanie hosta nie powoduje zmiany QQ
-IPTV_VERSION="122.01.15.00"
+IPTV_VERSION="122.01.16.00"
