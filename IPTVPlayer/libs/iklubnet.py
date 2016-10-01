56619b39eeeed65179a44b05f63269cb2e365ae2
samsamsam <samsamsam@o2.pl>
2016-10-01 11:49:33 +0200

    Fix some links extraction from http://iklub.net/

--- IPTVPlayer/libs/iklubnet.py
+++ IPTVPlayer/libs/iklubnet.py
@@ -56,7 +56,8 @@ class IKlubNetApi(CBaseHostClass):
         self.up = urlparser()
         self.http_params = {}
         self.http_params.update({'save_cookie': True, 'load_cookie': True, 'cookiefile': self.COOKIE_FILE})
-        self.cacheList = {}
+        #self.cacheList = {}
+        #self.cacheLinks = {'url':'', 'tab':[]}
         
     def getListOfChannels(self, cItem):
         printDBG("IKlubNetApi.getListOfChannels")
@@ -104,102 +105,100 @@ class IKlubNetApi(CBaseHostClass):
         
     def getVideoLink(self, cItem):
         printDBG("IKlubNetApi.getVideoLink")
-        
-        sts, data = self.cm.getPage(cItem['url'])
-        if not sts: return []
-        
-        urlNext = self.cm.ph.getSearchGroups(data, '<iframe[^>]+?src="([^"]+?)"', 1, True)[0]
-        if urlNext.startswith('http://') or urlNext.startswith('https://'):
-            sts, data = self.cm.getPage(urlNext)
-            if not sts: return []
-            
-        data = self.cm.ph.getAllItemsBeetwenMarkers(data, 'eval(', ');', False)
-        try:
-            ddata = ''
-            for idx in range(len(data)):
-                tmp = data[idx].split('+')
-                for item in tmp:
-                    item = item.strip()
-                    if item.startswith("'") or item.startswith('"'):
-                        ddata += self.cm.ph.getSearchGroups(item, '''['"]([^'^"]+?)['"]''')[0]
-                    else:
-                        tmp2 = re.compile('''unescape\(['"]([^"^']+?)['"]''').findall(item)
-                        for item2 in tmp2:
-                            ddata += urllib.unquote(item2)
-            
-            printDBG("++++++++++++++++++++++++++++++++++++++++++++++++++++")
-            printDBG(ddata)
-            printDBG("++++++++++++++++++++++++++++++++++++++++++++++++++++")
-            
-            funName = self.cm.ph.getSearchGroups(ddata, '''function\s*([^\(]+?)''')[0].strip()
-            sp      = self.cm.ph.getSearchGroups(ddata, '''split\(\s*['"]([^'^"]+?)['"]''')[0]
-            modStr  = self.cm.ph.getSearchGroups(ddata, '''\+\s*['"]([^'^"]+?)['"]''')[0] 
-            modInt  = int( self.cm.ph.getSearchGroups(ddata, '''\+\s*(-?[0-9]+?)[^0-9]''')[0] )
-            
-            ddata =  self.cm.ph.getSearchGroups(ddata, '''document\.write[^'^"]+?['"]([^'^"]+?)['"]''')[0]
-            data  = ''
-            tmp   = ddata.split(sp)
-            ddata = urllib.unquote(tmp[0])
-            k = urllib.unquote(tmp[1] + modStr)
-            for idx in range(len(ddata)):
-                data += chr((int(k[idx % len(k)]) ^ ord(ddata[idx])) + modInt)
-                
-            printDBG("++++++++++++++++++++++++++++++++++++++++++++++++++++")
-            printDBG(data)
-            printDBG("++++++++++++++++++++++++++++++++++++++++++++++++++++")
-            
-            if 'rtmp://' in data:
-                rtmpUrl = self.cm.ph.getDataBeetwenMarkers(data, '&source=', '&', False)[1]
-                return [{'name':'rtmp', 'url':rtmpUrl + ' live=1 '}]
-            elif 'content.jwplatform.com' in data:
-                vidUrl = self.getFullUrl( self.cm.ph.getSearchGroups(data, '''['"]([^'^"]+?content.jwplatform.com[^'^"]+?)['"]''')[0] )
-                
-                sts, data = self.cm.getPage(vidUrl)
-                if not sts: return []
-                
-                if '/players/' not in vidUrl:
-                    vidUrl = self.cm.ph.getSearchGroups(data, '''['"](https?[^'^"]+?/players/[^'^"]+?\.js)['"]''')[0]
-                    HEADER = dict( self.HEADER )
-                    HEADER['Referer'] = vidUrl
-                    sts, data = self.cm.getPage(vidUrl, {'header':HEADER})
-                    if not sts: return []
-                
-                data = self.cm.ph.getDataBeetwenMarkers(data, '"sources":', ']', False)[1]
-                file = self.cm.ph.getSearchGroups(data, r'''['"]?file['"]?\s*:\s*['"]([^"^']+)['"],''')[0]
-                printDBG(">>>>>>>>>>>>>>>>>>>>>>>> FILE[%s]" % file)
-                if file.startswith('http') and file.split('?')[-1].endswith('.m3u8'):
-                    return getDirectM3U8Playlist(file)
-                elif file.startswith('rtmp'):
-                    return [{'name':'rtmp', 'url':file + ' live=1 '}]
-        except:
-            printExc()
-            return []
-            
-        return []
-        
-        url = self.MAIN_URL + 'set_cookie.php'
-        sts, data = self.cm.getPage(url, http_params, {'url':vid_url})
-        if not sts: return []
-        
-        url = self.MAIN_URL + 'get_channel_url.php'
-        sts, data = self.cm.getPage(url, http_params, {'cid':cItem['cid']})
-        if not sts: return []
-        
-        try:
-            vid_url = byteify(json.loads(data))
-            vid_url = vid_url['url']
-        except:
-            vid_url = data
-        
         urlsTab = []
-        vid_url = vid_url.strip()
-        if vid_url.startswith('http://') and 'm3u8' in vid_url:
+        
+        url = cItem['url']
+        nextTitle = 'Podstawowy '
+        for linkIdx in range(2):
+            if '' == url: break
+            title = nextTitle
+            
+            sts, data = self.cm.getPage(url)
+            if not sts: return urlsTab
+            
+            url = self.getFullUrl( self.cm.ph.getSearchGroups(data, '<a href="([^"]+?)"[^>]*?><img[^>]*?alt="Zapasowy Player"', 1, True)[0] )
+            if '' == url: url = self.getFullUrl( self.cm.ph.getSearchGroups(data, '<a href="([^"]+?)"[^>]*?><img[^>]*?alt="[^"]*?vlc[^"]*?"', 1, True)[0] )
+            nextTitle = 'Zapasowy '
+            
+            printDBG(data)
+            
+            urlNext = self.cm.ph.getSearchGroups(data, '<iframe[^>]+?src="([^"]+?)"', 1, True)[0]
+            if urlNext.startswith('http://') or urlNext.startswith('https://'):
+                sts, data = self.cm.getPage(urlNext)
+                if not sts: continue
+            
+            data = self.cm.ph.getAllItemsBeetwenMarkers(data, 'eval(', ');', False)
             try:
-                sessid = self.cm.getCookieItem(self.COOKIE_FILE, 'sessid')
-                msec   = self.cm.getCookieItem(self.COOKIE_FILE, 'msec')
-                statid = self.cm.getCookieItem(self.COOKIE_FILE, 'statid')
-                url = strwithmeta(vid_url, {'Cookie':'sessid=%s; msec=%s; statid=%s;' % (sessid, msec, statid)})
-                urlsTab = getDirectM3U8Playlist(url)
+                ddata = ''
+                for idx in range(len(data)):
+                    tmp = data[idx].split('+')
+                    for item in tmp:
+                        item = item.strip()
+                        if item.startswith("'") or item.startswith('"'):
+                            ddata += self.cm.ph.getSearchGroups(item, '''['"]([^'^"]+?)['"]''')[0]
+                        else:
+                            tmp2 = re.compile('''unescape\(['"]([^"^']+?)['"]''').findall(item)
+                            for item2 in tmp2:
+                                ddata += urllib.unquote(item2)
+                
+                printDBG("++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                printDBG(ddata)
+                printDBG("++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                
+                funName = self.cm.ph.getSearchGroups(ddata, '''function\s*([^\(]+?)''')[0].strip()
+                sp      = self.cm.ph.getSearchGroups(ddata, '''split\(\s*['"]([^'^"]+?)['"]''')[0]
+                modStr  = self.cm.ph.getSearchGroups(ddata, '''\+\s*['"]([^'^"]+?)['"]''')[0] 
+                modInt  = int( self.cm.ph.getSearchGroups(ddata, '''\+\s*(-?[0-9]+?)[^0-9]''')[0] )
+                
+                ddata =  self.cm.ph.getSearchGroups(ddata, '''document\.write[^'^"]+?['"]([^'^"]+?)['"]''')[0]
+                data  = ''
+                tmp   = ddata.split(sp)
+                ddata = urllib.unquote(tmp[0])
+                k = urllib.unquote(tmp[1] + modStr)
+                for idx in range(len(ddata)):
+                    data += chr((int(k[idx % len(k)]) ^ ord(ddata[idx])) + modInt)
+                    
+                printDBG("++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                printDBG(data)
+                printDBG("++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                
+                if 'rtmp://' in data:
+                    rtmpUrl = self.cm.ph.getDataBeetwenMarkers(data, '&source=', '&', False)[1]
+                    urlsTab.append({'name':title + ' [rtmp]', 'url':rtmpUrl + ' live=1 '})
+                elif 'content.jwplatform.com' in data:
+                    vidUrl = self.getFullUrl( self.cm.ph.getSearchGroups(data, '''['"]([^'^"]+?content.jwplatform.com[^'^"]+?)['"]''')[0] )
+                    
+                    sts, data = self.cm.getPage(vidUrl)
+                    if not sts: continue
+                    
+                    if '/players/' not in vidUrl:
+                        vidUrl = self.cm.ph.getSearchGroups(data, '''['"](https?[^'^"]+?/players/[^'^"]+?\.js)['"]''')[0]
+                        HEADER = dict( self.HEADER )
+                        HEADER['Referer'] = vidUrl
+                        sts, data = self.cm.getPage(vidUrl, {'header':HEADER})
+                        if not sts: continue
+                    
+                    data = self.cm.ph.getDataBeetwenMarkers(data, '"sources":', ']', False)[1]
+                    file = self.cm.ph.getSearchGroups(data, r'''['"]?file['"]?\s*:\s*['"]([^"^']+)['"],''')[0]
+                    printDBG(">>>>>>>>>>>>>>>>>>>>>>>> FILE[%s]" % file)
+                    if file.startswith('http') and file.split('?')[-1].endswith('.m3u8'):
+                        urlsTab.extend( getDirectM3U8Playlist(file) )
+                    elif file.startswith('rtmp'):
+                        urlsTab.append({'name':title + ' [rtmp]', 'url':file + ' live=1 '})
+                elif 'tvp.info' in data:
+                    vidUrl = self.getFullUrl( self.cm.ph.getSearchGroups(data, '''['"](http[^'^"]+?tvp.info[^'^"]+?)['"]''')[0] )
+                    sts, data = self.cm.getPage(vidUrl)
+                    if not sts: continue
+                    printDBG(data)
+                    file = self.cm.ph.getSearchGroups(data, '''['"](https?[^'^"]+?\.m3u8[^'^"]*?)['"]''')[0]
+                    urlsTab.extend( getDirectM3U8Playlist(file) )
+                elif 'mrl=' in data:
+                    file = self.cm.ph.getSearchGroups(data, '''mrl=['"](http[^'^"]+?)['"]''')[0]
+                    urlsTab.append({'name':title + ' [mrl]', 'url':file})
+                else:
+                    urlsTab.extend( self.up.getAutoDetectedStreamLink(url, data) )
             except:
-                SetIPTVPlayerLastHostError("Problem z dostępem do pliku \"%\".\nSprawdź, czy masz wolne miejsce na pliki cookies." % self.COOKIE_FILE)
+                printExc()
+                continue
+        
         return urlsTab
