be49fecdf50ef5528315b7f818ec3cf51b5e90b6
samsamsam <samsamsam@o2.pl>
2017-03-25 14:25:20 +0100

    Add new host http://www.okgoals.com/

--- IPTVPlayer/version.py
+++ IPTVPlayer/version.py
@@ -7,4 +7,4 @@
 #zmiana YY powoduje wyzerowanie ZZ
 #zmiana XX powoduje wyzerowanie YY i ZZ
 #usunięcie/zablokowanie hosta nie powoduje zmiany QQ
-IPTV_VERSION="147.00.26.00"
+IPTV_VERSION="148.00.00.00"
