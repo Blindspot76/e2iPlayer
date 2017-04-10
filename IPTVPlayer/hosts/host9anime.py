615b14a6a6f3b4a24670d64f47dba16ca734cbab
samsamsam <samsamsam@o2.pl>
2017-04-10 22:02:14 +0200

    New host https://9anime.to/ has been added

---
+++ IPTVPlayer/hosts/host9anime.py
@@ -0,0 +1,406 @@
+# -*- coding: utf-8 -*-
+###################################################
+# LOCAL import
+###################################################
+from Plugins.Extensions.IPTVPlayer.components.iptvplayerinit import TranslateTXT as _, SetIPTVPlayerLastHostError
+from Plugins.Extensions.IPTVPlayer.components.ihost import CHostBase, CBaseHostClass, CDisplayListItem, RetHost, CUrlItem, ArticleContent
+from Plugins.Extensions.IPTVPlayer.tools.iptvtools import printDBG, printExc, CSearchHistoryHelper, remove_html_markup, GetLogoDir, GetCookieDir, byteify, rm
+from Plugins.Extensions.IPTVPlayer.libs.pCommon import common, CParsingHelper
+import Plugins.Extensions.IPTVPlayer.libs.urlparser as urlparser
+from Plugins.Extensions.IPTVPlayer.libs.youtube_dl.utils import clean_html
+from Plugins.Extensions.IPTVPlayer.tools.iptvtypes import strwithmeta
+###################################################
+
+###################################################
+# FOREIGN import
+###################################################
+import urlparse
+import time
+import re
+import urllib
+import string
+import random
+import base64
+from copy import deepcopy
+from hashlib import md5
+try:    import json
+except Exception: import simplejson as json
+from Components.config import config, ConfigSelection, ConfigYesNo, ConfigText, getConfigListEntry
+###################################################
+
+
+###################################################
+# E2 GUI COMMPONENTS 
+###################################################
+from Plugins.Extensions.IPTVPlayer.components.asynccall import MainSessionWrapper
+from Screens.MessageBox import MessageBox
+###################################################
+
+###################################################
+# Config options for HOST
+###################################################
+
+def GetConfigList():
+    optionList = []
+    return optionList
+###################################################
+
+def gettytul():
+    return 'https://9anime.to/'
+
+class AnimeTo(CBaseHostClass):
+ 
+    def __init__(self):
+        CBaseHostClass.__init__(self, {'history':'9anime.to', 'cookie':'9animeto.cookie', 'cookie_type':'MozillaCookieJar'})
+        self.DEFAULT_ICON_URL = 'http://redeneobux.com/wp-content/uploads/2017/01/2-4.png'
+        self.USER_AGENT = 'User-Agent=Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0'
+        self.HEADER = {'User-Agent': self.USER_AGENT, 'DNT':'1', 'Accept': 'text/html'}
+        self.AJAX_HEADER = dict(self.HEADER)
+        self.AJAX_HEADER.update( {'X-Requested-With': 'XMLHttpRequest'} )
+        self.MAIN_URL = 'https://9anime.to/'
+        self.cacheLinks    = {}
+        self.cacheFilters  = {}
+        self.cacheFiltersKeys = []
+        self.defaultParams = {'header':self.HEADER, 'use_cookie': True, 'load_cookie': True, 'save_cookie': True, 'cookiefile': self.COOKIE_FILE}
+    
+        self.MAIN_CAT_TAB = [{'category':'list_filters',    'title': _('Home'),        'url':self.getFullUrl('filter')        },
+                             {'category':'list_items',      'title': _('Newest'),      'url':self.getFullUrl('newest')        },
+                             {'category':'list_items',      'title': _('Last update'), 'url':self.getFullUrl('updated')       },
+                             {'category':'list_items',      'title': _('Most watched'),'url':self.getFullUrl('most-watched')  },
+                             #{'category':'list_items2',     'title': _('Upcoming'),   'url':self.getFullUrl('upcoming') },
+                             
+                             {'category':'search',            'title': _('Search'), 'search_item':True,},
+                             {'category':'search_history',    'title': _('Search history'),            } 
+                            ]
+                            
+    def getFullIconUrl(self, url):
+        url = url.replace('&amp;', '&')
+        return CBaseHostClass.getFullIconUrl(self, url)
+        
+    def getPage(self, baseUrl, addParams = {}, post_data = None):
+        if addParams == {}:
+            addParams = dict(self.defaultParams)
+        
+        def _getFullUrl(url):
+            if self.cm.isValidUrl(url):
+                return url
+            else:
+                return urlparse.urljoin(baseUrl, url)
+            
+        addParams['cloudflare_params'] = {'domain':self.up.getDomain(baseUrl), 'cookie_file':self.COOKIE_FILE, 'User-Agent':self.USER_AGENT, 'full_url_handle':_getFullUrl}
+        sts, data = self.cm.getPageCFProtection(baseUrl, addParams, post_data)
+        return sts, data
+    
+    def fillCacheFilters(self, cItem):
+        printDBG("AnimeTo.listCategories")
+        self.cacheFilters = {}
+        self.cacheFiltersKeys = []
+        
+        sts, data = self.getPage(cItem['url'])
+        if not sts: return
+        
+        def addFilter(data, marker, baseKey, addAll=True, titleBase=''):
+            key = 'f_' + baseKey
+            self.cacheFilters[key] = []
+            for item in data:
+                if ('name="%s"' % baseKey) not in item:
+                    continue
+                value = self.cm.ph.getSearchGroups(item, marker + '''="([^"]+?)"''')[0]
+                if value == '':
+                    continue
+                title = self.cleanHtmlStr(item)
+                if title.lower() in ['all', 'default', 'any']:
+                    addAll = False
+                self.cacheFilters[key].append({'title':title.title(), key:value})
+                
+            if len(self.cacheFilters[key]):
+                if addAll: self.cacheFilters[key].insert(0, {'title':_('All')})
+                self.cacheFiltersKeys.append(key)
+        
+        data = self.cm.ph.getAllItemsBeetwenMarkers(data, '<div class="filter dropdown">', '</ul>')
+        for tmp in data:
+            key = self.cm.ph.getSearchGroups(tmp, '''name="([^"]+?)"''')[0]
+            tmp = self.cm.ph.getAllItemsBeetwenMarkers(tmp, '<li', '</li>')
+            addFilter(tmp, 'value', key)
+        
+        printDBG(self.cacheFilters)
+        
+    def listFilters(self, cItem, nextCategory):
+        printDBG("AnimeTo.listFilters")
+        cItem = dict(cItem)
+        
+        f_idx = cItem.get('f_idx', 0)
+        if f_idx == 0: self.fillCacheFilters(cItem)
+        filter = self.cacheFiltersKeys[f_idx]
+        f_idx += 1
+        cItem['f_idx'] = f_idx
+        if f_idx  == len(self.cacheFiltersKeys):
+            cItem['category'] = nextCategory
+        self.listsTab(self.cacheFilters.get(filter, []), cItem)
+        
+    def listItems(self, cItem, nextCategory):
+        printDBG("AnimeTo.listItems")
+        url = cItem['url']
+        page = cItem.get('page', 1)
+        
+        query = {}
+        if page > 1: query['page'] = page
+        
+        for key in self.cacheFiltersKeys:
+            baseKey = key[2:] # "f_"
+            if key in cItem: query[baseKey] = urllib.quote(cItem[key])
+        
+        query = urllib.urlencode(query)
+        if '?' in url: url += '&' + query
+        else: url += '?' + query
+        
+        sts, data = self.getPage(url)
+        if not sts: return
+        
+        if  '>Next<' in data: nextPage = True
+        else: nextPage = False
+        
+        data = self.cm.ph.getAllItemsBeetwenMarkers(data, '<div class="item">', '</div>')
+        for item in data:
+            url = self.getFullUrl( self.cm.ph.getSearchGroups(item, 'href="([^"]+?)"')[0] )
+            tip = self.getFullUrl( self.cm.ph.getSearchGroups(item, 'data-tip="([^"]+?)"')[0] )
+            if not self.cm.isValidUrl(url): continue
+            icon = self.getFullIconUrl( self.cm.ph.getSearchGroups(item, 'src="([^"]+?)"')[0] )
+            title = self.cleanHtmlStr( item )
+            if title == '': title = self.cleanHtmlStr(self.cm.ph.getSearchGroups(item, '''alt=['"]([^'^"]+?)['"]''')[0])
+            if title == '': title = self.cleanHtmlStr(self.cm.ph.getSearchGroups(item, '''title=['"]([^'^"]+?)['"]''')[0])
+
+            params = dict(cItem)
+            params = {'good_for_fav': True, 'title':title, 'url':url, 'tip_url':tip, 'icon':icon}
+            params['category'] = nextCategory
+            self.addDir(params)
+        
+        if nextPage and len(self.currList) > 0:
+            params = dict(cItem)
+            params.update({'title':_("Next page"), 'page':page+1})
+            self.addDir(params)
+            
+    def exploreItem(self, cItem):
+        printDBG("AnimeTo.exploreItem")
+        
+        sts, data = self.getPage(cItem['url'])
+        if not sts: return
+        
+        titlesTab = []
+        self.cacheLinks  = {}
+        data = self.cm.ph.getDataBeetwenMarkers(data, '<div id="servers">', '<div class="widget')[1]
+        data = data.split('<div class="server row"')
+        for tmp in data:
+            serverName = self.cleanHtmlStr(self.cm.ph.getDataBeetwenMarkers(tmp, '<label', '</label>')[1])
+            tmp = self.cm.ph.getAllItemsBeetwenMarkers(tmp, '<li', '</li>')
+            for item in tmp:
+                title = self.cleanHtmlStr(item)
+                id    = self.cm.ph.getSearchGroups(item, '''data-id=['"]([^'^"]+?)['"]''')[0]
+                url   = self.getFullUrl(self.cm.ph.getSearchGroups(item, '''href=['"]([^'^"]+?)['"]''')[0])
+                if title not in titlesTab:
+                    titlesTab.append(title)
+                    self.cacheLinks[title] = []
+                url = strwithmeta(url, {'id':id})
+                self.cacheLinks[title].append({'name':serverName, 'url':url, 'need_resolve':1})
+        
+        for item in titlesTab:
+            params = dict(cItem)
+            params.update({'title':'%s : %s' % (cItem['title'], item), 'links_key':item})
+            self.addVideo(params)
+
+    def listSearchResult(self, cItem, searchPattern, searchType):
+        printDBG("AnimeTo.listSearchResult cItem[%s], searchPattern[%s] searchType[%s]" % (cItem, searchPattern, searchType))
+        cItem = dict(cItem)
+        cItem['url'] = self.getFullUrl('search?keyword=' + urllib.quote_plus(searchPattern))
+        self.listItems(cItem, 'explore_item')
+        
+    def getLinksForVideo(self, cItem):
+        printDBG("AnimeTo.getLinksForVideo [%s]" % cItem)
+        key = cItem.get('links_key', '')
+        return self.cacheLinks.get(key, [])
+        
+    def getVideoLinks(self, videoUrl):
+        printDBG("AnimeTo.getVideoLinks [%s]" % videoUrl)
+        videoUrl = strwithmeta(videoUrl)
+        urlTab = []
+        
+        # mark requested link as used one
+        if len(self.cacheLinks.keys()):
+            for key in self.cacheLinks:
+                for idx in range(len(self.cacheLinks[key])):
+                    if videoUrl in self.cacheLinks[key][idx]['url']:
+                        if not self.cacheLinks[key][idx]['name'].startswith('*'):
+                            self.cacheLinks[key][idx]['name'] = '*' + self.cacheLinks[key][idx]['name']
+                        break
+        
+        id = videoUrl.meta.get('id', '')
+        params = dict(self.defaultParams)
+        params['header'] = dict(self.AJAX_HEADER)
+        params['header']['Referer'] = str(videoUrl)
+        
+        sts, data = self.getPage('https://9anime.to/ajax/episode/info?id=%s&update=0' % id, params)
+        if not sts: return []
+        
+        videoUrl = ''
+        subTrack = ''
+        try:
+            data = byteify(json.loads(data))
+            subTrack = data.get('subtitle', '')
+            if data['type'] == 'iframe':
+                videoUrl = data['target']
+            elif data['type'] == 'direct':
+                query = dict(data['params'])
+                query.update({'mobile':'0'})
+                url = data['grabber'] + '?' + urllib.urlencode(query)
+                sts, data = self.getPage(url, params)
+                if not sts: return []
+                data = byteify(json.loads(data))
+                for item in data['data']:
+                    if item['type'] != 'mp4': continue
+                    if not self.cm.isValidUrl(item['file']): continue
+                    urlTab.append({'name':item['label'], 'url':item['file']})
+                urlTab = urlTab[::-1]
+            else:
+                printDBG('Unknown url type!')
+                printDBG(">>>>>>>>>>>>>>>>>>>>>")
+                printDBG(data)
+                printDBG("<<<<<<<<<<<<<<<<<<<<<")
+        except Exception:
+            printExc()
+
+        if self.cm.isValidUrl(videoUrl) and 0 == len(urlTab):
+            urlTab = self.up.getVideoLinkExt(videoUrl)
+        
+        if self.cm.isValidUrl(subTrack):
+            format = subTrack[-3:]
+            for idx in range(len(urlTab)):
+                urlTab[idx]['url'] = strwithmeta(urlTab[idx]['url'])
+                if 'external_sub_tracks' not in urlTab[idx]['url'].meta: urlTab[idx]['url'].meta['external_sub_tracks'] = []
+                urlTab[idx]['url'].meta['external_sub_tracks'].append({'title':'', 'url':subTrack, 'lang':'pt', 'format':format})
+        
+        return urlTab
+    
+    def getFavouriteData(self, cItem):
+        printDBG('AnimeTo.getFavouriteData')
+        params = {'type':cItem['type'], 'category':cItem.get('category', ''), 'title':cItem['title'], 'url':cItem['url'], 'desc':cItem['desc'], 'icon':cItem['icon']}
+        return json.dumps(params) 
+        
+    def getLinksForFavourite(self, fav_data):
+        printDBG('AnimeTo.getLinksForFavourite')
+        if self.MAIN_URL == None:
+            self.selectDomain()
+        links = []
+        try:
+            cItem = byteify(json.loads(fav_data))
+            links = self.getLinksForVideo(cItem)
+        except Exception: printExc()
+        return links
+        
+    def setInitListFromFavouriteItem(self, fav_data):
+        printDBG('AnimeTo.setInitListFromFavouriteItem')
+        if self.MAIN_URL == None:
+            self.selectDomain()
+        try:
+            params = byteify(json.loads(fav_data))
+        except Exception: 
+            params = {}
+            printExc()
+        self.addDir(params)
+        return True
+        
+    def getArticleContent(self, cItem):
+        printDBG("AnimeTo.getArticleContent [%s]" % cItem)
+        retTab = []
+        
+        sts, data = self.getPage(cItem.get('url', ''))
+        if not sts: return retTab
+        
+        desc = self.cleanHtmlStr(self.cm.ph.getDataBeetwenReMarkers(data, re.compile('<[^>]+?id="movie-synopsis"[^>]*?>'), re.compile('</div>'))[1])
+        if desc == '': desc  = self.cleanHtmlStr( self.cm.ph.getSearchGroups(data, '<meta property="og:description"[^>]+?content="([^"]+?)"')[0] )
+        
+        title = self.cleanHtmlStr( self.cm.ph.getSearchGroups(data, '<meta property="og:title"[^>]+?content="([^"]+?)"')[0] )
+        icon  = self.getFullUrl( self.cm.ph.getSearchGroups(data, '<meta property="og:image"[^>]+?content="([^"]+?)"')[0] )
+        
+        if title == '': title = cItem['title']
+        if desc == '':  title = cItem['desc']
+        if icon == '':  title = cItem['icon']
+        
+        descData = self.cm.ph.getDataBeetwenMarkers(data, '<div class="movie-detailed-info">', '<div class="clear">', False)[1] 
+        descTabMap = {"genre":         "genre",
+                      "year":          "year",
+                      "original-name": "alternate_title"}
+        
+        otherInfo = {}
+        tmp = self.cm.ph.getAllItemsBeetwenMarkers(descData, '<span', '</span>')
+        for item in tmp:
+            key = self.cm.ph.getSearchGroups(item, '''class=['"]([^'^"]+?)['"]''')[0]
+            if key in descTabMap:
+                try: otherInfo[descTabMap[key]] = self.cleanHtmlStr(item)
+                except Exception: continue
+                
+        status = self.cleanHtmlStr(self.cm.ph.getDataBeetwenMarkers(descData, 'Estado:', '</span>', False)[1])
+        if status != '': otherInfo['status'] = status
+        
+        year = self.cm.ph.getSearchGroups(self.cm.ph.getDataBeetwenMarkers(descData, '<span class="year">', '<span class="', False)[1], '[^0-9]([0-9]+?)[^0-9]')[0]
+        if year != '': otherInfo['year'] = year
+        
+        director = self.cleanHtmlStr(self.cm.ph.getDataBeetwenReMarkers(descData, re.compile('Realizador:\s*</span>'), re.compile('</span>'), False)[1])
+        if director != '': otherInfo['director'] = director
+        
+        creator = self.cleanHtmlStr(self.cm.ph.getDataBeetwenReMarkers(descData, re.compile('Criador:\s*</span>'), re.compile('</span>'), False)[1])
+        if creator != '': otherInfo['creator'] = creator
+        
+        actors = self.cleanHtmlStr(self.cm.ph.getDataBeetwenReMarkers(descData, re.compile('Elenco:\s*</span>'), re.compile('</span>'), False)[1])
+        if actors != '': otherInfo['actors'] = actors
+        
+        imdb_rating = self.cleanHtmlStr(self.cm.ph.getDataBeetwenMarkers(descData, '<div class="imdb-rate">', '</span>', False)[1])
+        if imdb_rating != '': otherInfo['imdb_rating'] = imdb_rating
+        
+        return [{'title':self.cleanHtmlStr( title ), 'text': self.cleanHtmlStr( desc ), 'images':[{'title':'', 'url':self.getFullUrl(icon)}], 'other_info':otherInfo}]
+        
+    def handleService(self, index, refresh = 0, searchPattern = '', searchType = ''):
+        printDBG('handleService start')
+        
+        CBaseHostClass.handleService(self, index, refresh, searchPattern, searchType)
+
+        name     = self.currItem.get("name", '')
+        category = self.currItem.get("category", '')
+        mode     = self.currItem.get("mode", '')
+        
+        printDBG( "handleService: |||||||||||||||||||||||||||||||||||| name[%s], category[%s] " % (name, category) )
+        self.currList = []
+        
+    #MAIN MENU
+        if name == None:
+            self.cacheLinks = {}
+            self.listsTab(self.MAIN_CAT_TAB, {'name':'category'})
+        elif category == 'list_filters':
+            self.listFilters(self.currItem, 'list_items')
+        elif category == 'list_items':
+            self.listItems(self.currItem, 'explore_item')
+        elif category == 'explore_item':
+            self.exploreItem(self.currItem)
+    #SEARCH
+        elif category in ["search", "search_next_page"]:
+            cItem = dict(self.currItem)
+            cItem.update({'search_item':False, 'name':'category'}) 
+            self.listSearchResult(cItem, searchPattern, searchType)
+    #HISTORIA SEARCH
+        elif category == "search_history":
+            self.listsHistory({'name':'history', 'category': 'search'}, 'desc', _("Type: "))
+        else:
+            printExc()
+        
+        CBaseHostClass.endHandleService(self, index, refresh)
+
+class IPTVHost(CHostBase):
+
+    def __init__(self):
+        CHostBase.__init__(self, AnimeTo(), True, [])
+        
+    def withArticleContent(self, cItem):
+        if cItem['type'] != 'video' and cItem['category'] != 'list_episodes' and cItem['category'] != 'list_seasons':
+            return False
+        return True
+    
+    
