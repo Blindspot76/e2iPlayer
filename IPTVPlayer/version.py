54eb814f14e47e6e18bb41bc4861c21f65bd7bd5
samsamsam <samsamsam@o2.pl>
2017-04-12 08:28:20 +0200

    Remove not working host http://www.icefilms.info/ - links are secured by captcha v2 with high security level

--- IPTVPlayer/version.py
+++ IPTVPlayer/version.py
@@ -7,4 +7,4 @@
 #zmiana YY powoduje wyzerowanie ZZ
 #zmiana XX powoduje wyzerowanie YY i ZZ
 #usunięcie/zablokowanie hosta nie powoduje zmiany QQ
-IPTV_VERSION="152.00.02.00"
+IPTV_VERSION="152.00.02.01"
