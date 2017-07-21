c5274c54f9640b3ad268bf6b625f6ece5d83cd89
Zdzislaw <zdzislaw22@windowslive.com>
2017-07-22 00:03:11 +0200

    delete script included by mistake

--- sync-repo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/iptvplayer-GitLab-j00zek-fork
-#git remote add original git@gitlab.com:iptvplayer-for-e2/iptvplayer-for-e2.git
-git fetch original
-git merge original/master
-git push
