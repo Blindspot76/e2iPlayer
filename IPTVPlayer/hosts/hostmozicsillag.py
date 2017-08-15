6c192ab595b4581cdb5ae1871ab899943f2c5de6
samsamsam <samsamsam@o2.pl>
2017-08-15 12:10:13 +0200

    New version of host Onet VOD PL -> https://vod.pl/

--- IPTVPlayer/hosts/hostmozicsillag.py
+++ IPTVPlayer/hosts/hostmozicsillag.py
@@ -53,7 +53,7 @@ class MuziCsillangCC(CBaseHostClass):
     def __init__(self):
         CBaseHostClass.__init__(self, {'history':'mozicsillag.cc', 'cookie':'mozicsillag.cc.cookie', 'cookie_type':'MozillaCookieJar'})
         self.DEFAULT_ICON_URL = 'http://mozicsillag.cc/img/logo.png'
-        self.USER_AGENT = 'User-Agent=Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0'
+        self.USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0'
         self.HEADER = {'User-Agent': self.USER_AGENT, 'DNT':'1', 'Accept': 'text/html'}
         self.AJAX_HEADER = dict(self.HEADER)
         self.AJAX_HEADER.update( {'X-Requested-With': 'XMLHttpRequest'} )
