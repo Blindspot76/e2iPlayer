93fcef2b1aa1e5ee0f7c1f8fa2efc92d6752b7f6
samsamsam <samsamsam@o2.pl>
2017-09-30 20:18:13 +0200

    Deploy new version hlsdl with fix for error "memory double free"

--- IPTVPlayer/setup/iptvsetupimpl.py
+++ IPTVPlayer/setup/iptvsetupimpl.py
@@ -99,7 +99,7 @@ class IPTVSetupImpl:
         self.subparserPaths = [resolveFilename(SCOPE_PLUGINS, 'Extensions/IPTVPlayer/libs/iptvsubparser/_subparser.so')]
         
         # hlsdl
-        self.hlsdlVersion = 0.12
+        self.hlsdlVersion = 0.13
         self.hlsdlPaths = [resolveFilename(SCOPE_PLUGINS, 'Extensions/IPTVPlayer/bin/hlsdl')]
         
         # duk
