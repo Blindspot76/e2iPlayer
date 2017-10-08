a8776a65426c425f51ce17aa6dbc618b8cb815c2
samsamsam <samsamsam@o2.pl>
2017-10-08 14:02:27 +0200

    http://vod.tvp.pl/ - fix TVP 3 live streams

--- IPTVPlayer/hosts/hosttvpvod.py
+++ IPTVPlayer/hosts/hosttvpvod.py
@@ -265,7 +265,7 @@ class TvpVod(CBaseHostClass):
         if not sts: return
         data = self.cm.ph.getAllItemsBeetwenMarkers(data, '<div class="button', '</div>', withMarkers=True, caseSensitive=False)
         for item in data:
-            id    = self.cm.ph.getSearchGroups(item, 'data-video_id="([0-9]+?)"')[0]
+            id    = self.cm.ph.getSearchGroups(item, 'data-video[_\-]id="([0-9]+?)"')[0]
             if id != '':
                 desc  = self.cleanHtmlStr(self.cm.ph.getSearchGroups(item, 'titlte="([^"]+?)"')[0])
                 icon  = self.cm.ph.getSearchGroups(item, 'src="(http[^"]+?)"')[0]
