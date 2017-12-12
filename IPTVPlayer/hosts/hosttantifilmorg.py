fe2f9ff9c90f7719dd6b8964718334b8509571ba
samsamsam <samsamsam@o2.pl>
2017-12-12 18:39:15 +0100

    Fix tantifilm url

--- IPTVPlayer/hosts/hosttantifilmorg.py
+++ IPTVPlayer/hosts/hosttantifilmorg.py
@@ -58,7 +58,7 @@ class TantiFilmOrg(CBaseHostClass):
         self.cm.HEADER = self.HEADER # default header
         self.defaultParams = {'header':self.HEADER, 'use_cookie': True, 'load_cookie': True, 'save_cookie': True, 'cookiefile': self.COOKIE_FILE}
         
-        self.MAIN_URL = 'http://tantifilm.uno/'
+        self.MAIN_URL = 'http://www.tantifilm.uno/'
         self.DEFAULT_ICON_URL = 'https://raw.githubusercontent.com/Zanzibar82/images/master/posters/tantifilm.png'
         
         self.MAIN_CAT_TAB = [{'category':'list_categories',    'title': _('Categories'),                           'url':self.MAIN_URL  },
