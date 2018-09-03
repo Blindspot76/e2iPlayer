0b8d8be789f77209b7b854397aaf4c49c20b4289
samsamsam <samsamsam@o2.pl>
2018-09-03 02:09:44 +0200

    Another fix

--- IPTVPlayer/hosts/hostvumedicom.py
+++ IPTVPlayer/hosts/hostvumedicom.py
@@ -226,7 +226,7 @@ class VUMEDI(CBaseHostClass):
             url = self.getFullUrl(self.cm.ph.getSearchGroups(tmp, '''\shref=['"]([^'^"]+?)['"]''')[0].strip())
             if url == '': continue
             
-            icon = self.cm.ph.getSearchGroups(item, '''\ssrc=['"]([^"^']+?\.(?:jpe?g|png)(?:\?[^'^"]*?)?)['"]''')[0]
+            icon = self.getFullIconUrl(self.cm.ph.getSearchGroups(item, '''\ssrc=['"]([^"^']+?\.(?:jpe?g|png)(?:\?[^'^"]*?)?)['"]''')[0])
             title = self.cleanHtmlStr(tmp)
             
             desc = [t]
