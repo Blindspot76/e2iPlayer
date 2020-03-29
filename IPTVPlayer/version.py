6fbfb30693ae6778e0b0097e1d0bcba86be74f4d
Blindspot76 <42062762+Blindspot76@users.noreply.github.com>
2020-03-29 14:18:09 +0200

    Add files via upload

--- IPTVPlayer/version.py
+++ IPTVPlayer/version.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 #YYYY.MM.DD.DAY_RELEASE
-IPTV_VERSION="2020.03.27.01"
+IPTV_VERSION="2020.03.29.01"
 
-=======
-# -*- coding: utf-8 -*-
-#YYYY.MM.DD.DAY_RELEASE
-IPTV_VERSION="2020.03.26.01"
-
->>>>>>> 118e2cccb360e09e0145881d923120252d7a0f46
