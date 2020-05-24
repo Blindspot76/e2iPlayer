e163391a9ab214717838114e07dcdb08d82dd31f
Blindspot76 <42062762+Blindspot76@users.noreply.github.com>
2020-05-24 21:37:18 +0200

    CinemaLibero javítva

--- IPTVPlayer/hosts/hostcinemalibero.py
+++ IPTVPlayer/hosts/hostcinemalibero.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+#
+# Host by Mark - Thanks to my bro, and to Maxbambi
+# 
 # typical import for a standard host
 ###################################################
 # LOCAL import
@@ -85,7 +88,6 @@ class Cinemalibero(CBaseHostClass):
     def listMainMenu(self, cItem):   
         printDBG('Cinemalibero.listMainMenu')
         MAIN_CAT_TAB = [{'category':'list_items',      'title': _('Movies') , 'url' : self.getFullUrl('/category/film') },
-                        {'category':'list_items',      'title': _('Popular'), 'url' : self.getFullUrl('/popularni-online-filmovi-sa-prevodom') },
                         {'category':'list_items',      'title': _('Series') , 'url': self.getFullUrl('/category/serie-tv') }    ,
                         {'category':'search',          'title': _('Search'),    'search_item':True, },
                         {'category':'search_history',  'title': _('Search history'),     }]
@@ -99,7 +101,9 @@ class Cinemalibero(CBaseHostClass):
         if page > 1:
             tmp = url.split('?')
             url = tmp[0]
-            if not url.endswith('/'): url += '/'
+            if not url.endswith('/'): 
+                url += '/'
+            
             url += 'page/%s/' % (page)
             if len(tmp) == 2: url += '?' + tmp[1]
         
@@ -111,28 +115,24 @@ class Cinemalibero(CBaseHostClass):
         else:
             nextPage = False
         
-        #tmp = self.cm.ph.getDataBeetwenMarkers(data, '<div class="card shadow border-0 mb-4"', '</div>')[1]
-        #tmp = self.cm.ph.getDataBeetwenMarkers(data, '<div class="container"', '</div>', False)[2]
         tmp = self.cm.ph.getDataBeetwenMarkers(data, ('<div','>','class="card shadow border-0 mb-4"') , ('<div', '>', 'class="container"'), False)[1]
         printDBG(tmp)
+
         if not tmp:
             tmp = self.cm.ph.getDataBeetwenMarkers(data, '<div id="content" role="main">' , ('<div', '>', 'nav-previous'), False)[1]
         if not tmp:
             tmp = self.cm.ph.getDataBeetwenMarkers(data, ('<main','>','main'), ('<div', '>', 'nav-previous'), False)[1]
-        
-        #next_page = self.cm.ph.getDataBeetwenMarkers(data, '<a class="next page-numbers"', '</a>')[1]
-        #printDBG('Cinemalibero.nextPage %s' % next_page)
-        #next_url = self.cm.ph.getSearchGroups(next_page, "href=['\"]([^'^\"]+?)['\"]")[0]
-        #printDBG('Cinemalibero.nextPageUrl %s' % next_url)
-        #next_params = {'title': _('Next page'), 'url': next_url}
-        #self.addDir(next_params)
+
+        if not tmp:
+            tmp = self.cm.ph.getDataBeetwenMarkers(data, '<div class="container">', '<footer>', False)[1]
         
         movies = self.cm.ph.getAllItemsBeetwenMarkers(tmp,'<a','</a>')
         
         for m in movies:
             title = self.cleanHtmlStr(self.cm.ph.getDataBeetwenMarkers(m, '<div class="titolo"', '</div>')[1])
-            #title = self.cleanHtmlStr(self.cm.ph.getDataBeetwenMarkers(m, '<div class="card-body p-0"', '</a>')[1])
-            #title = self.cm.ph.getSearchGroups(m, "title=['\"]([^'^\"]+?)['\"]")[0]
+            if not title:
+                continue
+            
             url = self.cm.ph.getSearchGroups(m, "href=['\"]([^'^\"]+?)['\"]")[0]
             icon = self.cm.ph.getSearchGroups(m, "background-image: url['\(]([^'^\"]+?)['\)]")[0]
             params = {'category':'explore_item', 'title': title, 'icon': icon , 'url': url}
@@ -141,52 +141,89 @@ class Cinemalibero(CBaseHostClass):
             
         if nextPage:
             params = dict(cItem)
-            params.update({'good_for_fav': False, 'title':_('Next page'), 'page':page+1})
-            self.addDir(params)
+            params.update({'category':'list_items', 'good_for_fav': False, 'title':_('Next page'), 'page':page+1})
+            self.addMore(params)
             
     def exploreItems (self, cItem):
         printDBG('Cinemalibero.exploreItem %s' % cItem)
         url = cItem['url']
                         
         sts, data = self.getPage(url)
-                        
+        #printDBG(data)                
         if not sts:
             return
         
         # check if is a series
-        tmp_seasons = self.cm.ph.getDataBeetwenMarkers(data, '<div bis_skin_checked' , '<div style', False)[1]
-        printDBG(tmp_seasons)
-        if tmp_seasons:
+        is_serie = self.cleanHtmlStr(self.cm.ph.getDataBeetwenMarkers(data, '<a href="https://cinemalibero.plus/category/serie-tv/" rel="category tag">', '</a>')[1])
+        #printDBG(is_serie)
+        if 'Serie' in is_serie:
             # it is a series
-            #printDBG(tmp_seasons)
+            seasons={}
+            tmp_seasons = self.cm.ph.getDataBeetwenMarkers(data, '<div class="at-above-post addthis_tool"', '<div class="at-below-post addthis_tool"')[1]
             tmp_seasons = tmp_seasons.split('<strong>')
             if len(tmp_seasons)>1:
                 del(tmp_seasons[0])
+            
+            #printDBG("tmp_seasons: %s" % tmp_seasons)
+            
             for s in tmp_seasons:
-                s = '<strong>' +s
-                seasonName = self.cm.ph.getDataBeetwenMarkers(data, '<strong>' , '</strong>', False)[1]
-                printDBG("Season name: %s" % seasonName)
-                eps = self.cm.ph.getAllItemsBeetwenMarkers(s,('<p','>'),'</p>') 
+                s = '<strong>' + s
+                seasonName = self.cleanHtmlStr(self.cm.ph.getDataBeetwenMarkers(s, '<strong>' , '</strong>', False)[1])
+                seasons[seasonName]=[]
+                printDBG("new season: '%s'" % seasonName)
+                
+                #eps = self.cm.ph.getAllItemsBeetwenMarkers(s, '&#215;', '<br />')
+                eps = s.split('<br />')
+                
                 for ep in eps:
                     printDBG('----------------------')
                     printDBG(ep)
                     printDBG('----------------------')
-                    epName = self.cleanHtmlStr(ep)
-                    # data-open="ssksk" data-very="sjsjs" data-only="sssh"
+
+                    i1 = ep.find('<p>')
+                    if i1 > 0:
+                        ep = ep[i1+3:]
+                    ep = ep.strip().encode('utf-8')
+                    
+                    if '&#215;' not in ep:
+                        continue
+                    
+                    printDBG('-----------dopo modifiche -----------')
+                    printDBG(ep)
+                    printDBG('-------------------------------------')
+                    
+                    
+                    epName = self.cleanHtmlStr(ep.split('<a')[0])
+                    epName = epName.replace('\xe2\x80\x93',"-")
+
+                    if epName.endswith("-"): 
+                        epName = epName[:-1]
+                    
+                    printDBG("new episode: '%s' of season '%s' " % (epName, seasonName))
+                    
                     linksTab = []
-                    video_id_openload = self.cm.ph.getSearchGroups(ep, "data-open=['\"]([^'^\"]+?)['\"]")[0]
+
+                    links = self.cm.ph.getAllItemsBeetwenMarkers(ep,'<a target','</a>')
+                    for l in links:
+                        url = self.cm.ph.getSearchGroups(l, "href=['\"]([^'^\"]+?)['\"]")[0]
+                        
+                        host = self.cleanHtmlStr(l)
+                        host = host.strip().encode('utf-8').replace('\xe2\x80\x93',"-")
+                        if host.startswith("-"): 
+                            host = host[1:]
+                        
+                        #printDBG("url: %s" % url)
+                        #printDBG("host: %s" % host)
+                        #printDBG("epname: %s" % epName)
+                        title = "%s - %s - %s" % (seasonName, epName, host)
+                        params = MergeDicts(cItem, {'title': title, 'url' : url})
+                        printDBG(str(params))
+                        linksTab.append(params)
+
+                    seasons[seasonName].append({'title': epName, 'category':'episode','links':linksTab})
+
                     
-                    video_id_verystream = self.cm.ph.getSearchGroups(ep, "data-very=['\"]([^'^\"]+?)['\"]")[0]
-                    
-                    video_id_onlystream = self.cm.ph.getSearchGroups(ep, "data-only=['\"]([^'^\"]+?)['\"]")[0]
-                    if video_id_onlystream:
-                        url_onlystream="https://onlystream.tv/e/%s" % video_id_onlystream
-                        linksTab.append({'name':'Onlystream', 'url': url_onlystream}) 
-                    
-                    title = "%s %s %s" % (cItem['title'], seasonName, epName)
-                    params = MergeDicts(cItem, {'title': title, 'url' : cItem['url'], 'links_tab': linksTab})
-                    printDBG(params)
-                    self.addVideo(params)
+                self.addDir({'category':'season','episodes_list': seasons[seasonName], 'title': seasonName})
                     
         else:
             #it is a movie
@@ -203,13 +240,28 @@ class Cinemalibero(CBaseHostClass):
                     printDBG(str(params))
                     self.addVideo(params)
         
+    def listEpisodes(self, cItem):
+        printDBG("Cinemalibero.listEpisodes cItem[%s]" % cItem)
+
+        for e in cItem.get('episodes_list',[]):
+            printDBG(json_dumps(e))
+            epName = e.get('title','')
+            if epName:
+                self.addDir ({'category':'episode','links': e['links'], 'title': epName})
+
+    def exploreEpisode(self, cItem):
+        printDBG("Cinemalibero.exploreEpisode cItem[%s]" % cItem)
+    
+        for l in cItem.get('links',[]):
+            printDBG(json_dumps(l))
+            self.addVideo(l)
+            
     def listSearchResult(self, cItem, searchPattern, searchType):
         printDBG("Cinemalibero.listSearchResult cItem[%s], searchPattern[%s] searchType[%s]" % (cItem, searchPattern, searchType))
         cItem = dict(cItem)
         cItem['url'] = self.getFullUrl('/?s=') + urllib.quote_plus(searchPattern)
         cItem['category'] = 'search_items'
         self.listItems(cItem)
-
                         
     def handleService(self, index, refresh = 0, searchPattern = '', searchType = ''):
         printDBG('Cinemalibero.handleService start')
@@ -229,6 +281,10 @@ class Cinemalibero(CBaseHostClass):
             self.listItems(self.currItem)
         elif category == 'explore_item':
             self.exploreItems(self.currItem)       
+        elif category == 'season':
+            self.listEpisodes(self.currItem)
+        elif category == 'episode':
+            self.exploreEpisode(self.currItem)
         #SEARCH
         elif category in ["search", "search_next_page"]:
             cItem = dict(self.currItem)
