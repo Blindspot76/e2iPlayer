1db81d2aceab7885c29dc405ddbdad9620fd5d8a
Blindspot76 <42062762+Blindspot76@users.noreply.github.com>
2020-06-03 09:55:26 +0200

    Frissítések, javítások

--- IPTVPlayer/iptvupdate/custom/info.sh
+++ IPTVPlayer/iptvupdate/custom/info.sh
@@ -2,7 +2,7 @@
 cp $1/iptvupdate/custom/info.sh $2/iptvupdate/custom/info.sh
 status=$?
 if [ $status -ne 0 ]; then
-	echo "Végzetes hiba. A $0 fájl nem másolható, error[$status]."
+	echo "Błąd krytyczny. Plik $0 nie może zostać skopiowany, error[$status]."
 	exit 1
 fi
 cp $1/hosts/hostinfoversion.py $2/hosts/
@@ -10,9 +10,9 @@ cp $1/icons/logos/infologo.png $2/icons/logos/
 cp $1/icons/PlayerSelector/infoversion*.png $2/icons/PlayerSelector/
 status=$?
 if [ $status -ne 0 ]; then
-	echo "Figyelem! A frissítés meghiúsult, error[$status]."
+	echo "Uwaga nie udalo sie skopiowac info, error[$status]."
 else
-	echo "Frissítés végrehajtva."
+	echo "Kopiowanie info OK"
 fi
 if [ -x /usr/bin/fullwget ] ; then
 	/usr/bin/fullwget --no-check-certificate https://gitlab.com/mosz_nowy/infoversion/repository/archive.tar.gz?ref=master -q -O /tmp/infoversion.tar.gz
@@ -26,9 +26,9 @@ fi
 		cp -r -f /tmp/infoversion-master*/iptvupdate/custom/* $2/iptvupdate/custom/
 		cp -r -f /tmp/infoversion-master*/hosts/* $2/hosts/
 		rm -r -f /tmp/infoversion*
-		echo "Nyomj OK-t, ha indulhat a frissítés"
+		echo "Download info tar.gz OK"
 	else
-		echo "Figyelem! A frissítés nem lesz végrehajtva!"
+		echo "Uwaga, Nie udał się Download info tar.gz"
 	fi
-echo "Frissítés siekresen végrehajtva."
+echo "Wykonywanie $0 zakończone sukcesem."
 exit 0
