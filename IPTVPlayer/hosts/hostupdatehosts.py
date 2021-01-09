60ff0fd2659f48960b85eb08d8bf228577e7aea9
Blindspot76 <42062762+Blindspot76@users.noreply.github.com>
2021-01-09 20:47:53 +0100

    Kisebb módosítások

--- IPTVPlayer/hosts/hostupdatehosts.py
+++ IPTVPlayer/hosts/hostupdatehosts.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ###################################################
-# 2019-12-27 by Alec - updatehosts HU host telepítő
+# 2021-01-09 by Blindspot - updatehosts HU host telepítő
 ###################################################
-HOST_VERSION = "4.1"
+HOST_VERSION = "4.2"
 ###################################################
 # LOCAL import
 ###################################################
@@ -138,8 +138,7 @@ class updatehosts(CBaseHostClass):
         self.WEBHUPLAYER = zlib.decompress(base64.b64decode('eJwrT03KKC3ISaxMLQIAG+YEqQ=='))
         self.AUTOHU = zlib.decompress(base64.b64decode('eJxLLC3JzygFAAj3Apc='))
         self.M4SPORT = zlib.decompress(base64.b64decode('eJzLNSkuyC8qAQAK3gLa'))
-        self.VIDEA = zlib.decompress(base64.b64decode('eJwry0xJTQQABk4CCg=='))
-        self.FILMGO = zlib.decompress(base64.b64decode('eJxLy8zJTc8HAAirAn8='))
+        self.VIDEA = zlib.decompress(base64.b64decode('eJwry0xJTQQABk4CCg=='))        
         self.NONSTOPMOZI = zlib.decompress(base64.b64decode('eJzLy88rLskvyM2vygQAHOUE0Q=='))
         self.TECHNIKA = zlib.decompress(base64.b64decode('eJwrSU3OyMvMTgQADu8DSA=='))
         self.aid = config.plugins.iptvplayer.updatehosts_id.value
@@ -279,7 +278,6 @@ class updatehosts(CBaseHostClass):
                 HOST_CAT_TAB.append(self.menuItem(self.AUTOHU))
                 HOST_CAT_TAB.append(self.menuItem(self.M4SPORT))
                 HOST_CAT_TAB.append(self.menuItem(self.VIDEA))
-                HOST_CAT_TAB.append(self.menuItem(self.FILMGO))
                 HOST_CAT_TAB.append(self.menuItem(self.NONSTOPMOZI))
                 HOST_CAT_TAB.append(self.menuItem(self.TECHNIKA))
                 HOST_CAT_TAB = sorted(HOST_CAT_TAB, key=lambda i: (i['azon'], i['title']))
@@ -316,8 +314,6 @@ class updatehosts(CBaseHostClass):
                 ls = '\n(Az m4sport.hu sport műsorait jeleníti meg - Boxutca, Magyar foci, UEFA Bajnokok Ligája foci, Sporthírek, Sportközvetítések)'
             if host == self.VIDEA:
                 ls = '\n(A videa.hu videóit jeleníti meg különböző kategóriákban, csatornákban)'
-            if host == self.FILMGO:
-                ls = '\n(A filmgo.cc tartalmait jeleníti meg. Filmek és sorozatok minden mennyiségben)'
             if host == self.NONSTOPMOZI:
                 ls = '\n(A nonstopmozi.com tartalmait jeleníti meg. Filmek és sorozatok minden mennyiségben)'
             if host == self.TECHNIKA:
@@ -558,9 +554,6 @@ class updatehosts(CBaseHostClass):
             elif tabID == self.VIDEA:
                 self.susn('2', '9', 'host_' + tabID)
                 self.host_telepites(self.VIDEA,True,False,'https://videa.hu')
-            elif tabID == self.FILMGO:
-                self.susn('2', '9', 'host_' + tabID)
-                self.host_telepites(self.FILMGO,True,False,'https://filmgo.cc')
             elif tabID == self.NONSTOPMOZI:
                 self.susn('2', '9', 'host_' + tabID)
                 self.host_telepites(self.NONSTOPMOZI,True,False,'https://nonstopmozi.com')
@@ -1833,7 +1826,7 @@ class updatehosts(CBaseHostClass):
         self.addDir(params)
         
     def herzs(self):
-        hlt = [self.UPDATEHOSTS,self.SONYPLAYER,self.MYTVTELENOR,self.RTLMOST,self.MINDIGO,self.MOZICSILLAG,self.FILMEZZ,self.WEBHUPLAYER,self.AUTOHU,self.M4SPORT,self.VIDEA,self.FILMGO,self.NONSTOPMOZI,self.TECHNIKA]        
+        hlt = [self.UPDATEHOSTS,self.SONYPLAYER,self.MYTVTELENOR,self.RTLMOST,self.MINDIGO,self.MOZICSILLAG,self.FILMEZZ,self.WEBHUPLAYER,self.AUTOHU,self.M4SPORT,self.VIDEA,self.NONSTOPMOZI,self.TECHNIKA]        
         try:
             for host in hlt:
                 lhv_tmp = self.getHostVersion_local(self.IH + self.HS + '/host' + host + '.py')
