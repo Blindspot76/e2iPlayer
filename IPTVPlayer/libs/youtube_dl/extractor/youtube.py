35fb3bd8090e117616d2c5a23d253a2920620061
Blindspot76 <42062762+Blindspot76@users.noreply.github.com>
2023-04-29 21:39:03 +0200

    Fix Youtube player

--- IPTVPlayer/libs/youtube_dl/extractor/youtube.py
+++ IPTVPlayer/libs/youtube_dl/extractor/youtube.py
@@ -491,33 +491,41 @@ class YoutubeIE(object):
             videoInfoparams = {'cookiefile': COOKIE_FILE, 'use_cookie': True, 'load_cookie': False, 'save_cookie': True}
             sts, video_webpage = self.cm.getPage(url)
         else:
-            url = 'https://www.youtube.com/youtubei/v1/player?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8'
-            isGoogleDoc = False
-            videoKey = 'video_id'
-            videoInfoparams = {}
-            http_params = {'header': {'Content-Type': 'application/json', 'Origin': 'https://www.youtube.com', 'X-YouTube-Client-Name': '3', 'X-YouTube-Client-Version': '16.20'}}
-            http_params['raw_post_data'] = True
-            post_data = "{'videoId': '%s', 'context': {'client': {'hl': 'en', 'clientVersion': '16.20', 'clientName': 'ANDROID'}}}" % video_id
-            sts, video_webpage = self.cm.getPage(url, http_params, post_data)
-            if sts:
-                if allowAgeGate and 'LOGIN_REQUIRED' in video_webpage:
-                    post_data = "{'videoId': '%s', 'thirdParty': 'https://google.com', 'context': {'client': {'hl': 'en', 'clientScreen': 'EMBED', 'clientVersion': '16.20', 'clientName': 'ANDROID'}}}" % video_id
-                    sts, video_webpage = self.cm.getPage(url, http_params, post_data)
-                player_response = json_loads(video_webpage)
-            else:
-                url = 'http://www.youtube.com/watch?v=%s&bpctr=9999999999&has_verified=1&' % video_id
-                sts, video_webpage = self.cm.getPage(url)
-                if sts:
-                    player_response = self._extract_yt_initial_variable(
+            tries = 0
+            while tries < 3:
+                tries+=1
+            	url = 'https://www.youtube.com/youtubei/v1/player?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8'
+            	isGoogleDoc = False
+            	videoKey = 'video_id'
+            	videoInfoparams = {}
+                http_params = {'header': {'User-Agent': 'com.google.android.youtube/17.31.35 (Linux; U; Android 11) gzip', 'Content-Type': 'application/json', 'Origin': 'https://www.youtube.com', 'X-YouTube-Client-Name': '3', 'X-YouTube-Client-Version': '17.31.35'}}
+            	http_params['raw_post_data'] = True
+                post_data = "{'videoId': '%s', 'context': {'client': {'hl': 'en', 'clientVersion': '17.31.35', 'clientName': 'ANDROID', 'androidSdkVersion': 30}}}" % video_id
+            	sts, video_webpage = self.cm.getPage(url, http_params, post_data)
+            	if sts:
+                    if allowAgeGate and 'LOGIN_REQUIRED' in video_webpage:
+                        http_params['header']['X-YouTube-Client-Name'] = '85'
+                        post_data = "{'videoId': '%s', 'thirdParty': 'https://google.com', 'context': {'client': {'clientName': 'TVHTML5_SIMPLY_EMBEDDED_PLAYER', 'clientVersion': '2.0', 'clientScreen': 'EMBED'}}}" % video_id
+                        sts, video_webpage = self.cm.getPage(url, http_params, post_data)
+                	printDBG(video_webpage)
+                    player_response = json_loads(video_webpage)
+                else:
+                    url = 'http://www.youtube.com/watch?v=%s&bpctr=9999999999&has_verified=1&' % video_id
+                    sts, video_webpage = self.cm.getPage(url)
+                    if sts:
+                        player_response = self._extract_yt_initial_variable(
                         video_webpage, self._YT_INITIAL_PLAYER_RESPONSE_RE,
                         video_id, 'initial player response')
-
+                printDBG("_real_extract tries %s" % tries)
+                if player_response['playabilityStatus']['status'] == 'OK':
+                    break
+        
         if not sts:
             raise ExtractorError('Unable to download video webpage')
 
         if not player_response:
             raise ExtractorError('Unable to get player response')
-
+	printDBG(player_response)
         video_info = player_response['videoDetails']
         # subtitles
         if 'lengthSeconds' not in video_info:
